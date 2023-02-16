7f9e29fb75281cbe38fbc8cebc10cbcdcf71d28d
Alexander Puzynia <werwolf.by@gmail.com>
2023-02-16 03:10:56 -0800

    refactor: extract Traverse from FolderInfor into Sortcery.Engine project

---
+++ Sortcery.Engine/Traverser.cs
@@ -0,0 +1,39 @@
+﻿using Mono.Unix;
+using Sortcery.Model;
+using FileInfo = Sortcery.Model.FileInfo;
+
+namespace Sortcery.Engine;
+
+public class Traverser
+{
+    public IReadOnlyDictionary<HardLinkId, FileInfo> Traverse(FolderInfo dir)
+    {
+        var files = new Dictionary<HardLinkId, FileInfo>();
+        var unixDir = new UnixDirectoryInfo(dir.FullName);
+        Traverse(dir, unixDir, files);
+        return files;
+    }
+
+    private void Traverse(FolderInfo dir, UnixDirectoryInfo currentDir, Dictionary<HardLinkId,FileInfo> result)
+    {
+        // Traverse all over GetSystemEntries() to get all files and directories
+        foreach (var entry in currentDir.GetFileSystemEntries())
+        {
+            switch (entry)
+            {
+                // If it's a directory, traverse it
+                case UnixDirectoryInfo subDir:
+                    Traverse(dir, subDir, result);
+                    break;
+                case UnixFileInfo file:
+                    // If it's a file, add it to the result
+                    var fileInfo = FileInfo.FromUnixFileInfo(dir, file);
+                    if (!result.ContainsKey(fileInfo.HardLinkId))
+                    {
+                        result.Add(fileInfo.HardLinkId, fileInfo);
+                    }
+                    break;
+            }
+        }
+    }
+}
--- Sortcery.Model/FolderInfo.cs
+++ Sortcery.Model/FolderInfo.cs
@@ -10,35 +10,4 @@ public class FolderInfo
     }
     
     public string FullName { get; }
-    
-    public IReadOnlyDictionary<HardLinkId, FileInfo> Traverse()
-    {
-        var result = new Dictionary<HardLinkId, FileInfo>();
-        var path = new UnixDirectoryInfo(FullName);
-        Traverse(result, path);
-        return result;
-    }
-    
-    private void Traverse(Dictionary<HardLinkId, FileInfo> result, UnixDirectoryInfo currentDir)
-    {
-        // Traverse all over GetSystemEntries() to get all files and directories
-        foreach (var entry in currentDir.GetFileSystemEntries())
-        {
-            switch (entry)
-            {
-                // If it's a directory, traverse it
-                case UnixDirectoryInfo subDir:
-                    Traverse(result, subDir);
-                    break;
-                case UnixFileInfo file:
-                    // If it's a file, add it to the result
-                    var fileInfo = FileInfo.FromUnixFileInfo(this, file);
-                    if (!result.ContainsKey(fileInfo.HardLinkId))
-                    {
-                        result.Add(fileInfo.HardLinkId, fileInfo);
-                    }
-                    break;
-            }
-        }
-    }
 }
