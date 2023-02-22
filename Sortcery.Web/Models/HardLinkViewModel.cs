ba2d7401d046b8eca0c3b77e527d8e5b123f2164
Alexander Puzynia <werwolf.by@gmail.com>
2023-02-22 01:15:53 -0800

    feat: add HardLinkViewModel for Web

---
+++ Sortcery.Web/Models/HardLinkViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Sortcery.Api.Contracts.Models;
+
+namespace Sortcery.Web.Models
+{
+    public class HardLinkViewModel
+    {
+        private readonly HardLinkInfo _hardLinkInfo;
+
+        public HardLinkViewModel(HardLinkInfo hardLinkInfo)
+        {
+            _hardLinkInfo = hardLinkInfo;
+        }
+
+        public FileInfo? Source => _hardLinkInfo.Source;
+
+        public List<FileInfo> Targets => _hardLinkInfo.Targets;
+
+        public FileInfo? Guess { get; set; }
+
+        public bool Guessing { get; set; }
+
+        public bool CanGuess => _hardLinkInfo.Targets.Count == 0 && Guess == null;
+
+        public bool CanLink => Guess != null;
+
+        public IEnumerable<FileInfo> AllTargets
+        {
+            get
+            {
+                foreach (var target in Targets)
+                {
+                    yield return target;
+                }
+
+                if (Guess != null)
+                {
+                    yield return Guess;
+                }
+            }
+        }
+    }
+}
